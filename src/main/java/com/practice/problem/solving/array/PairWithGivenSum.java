6e12c75b948c165e356801a49d2c367c7e16a945
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2023-09-27 22:20:09 +0530

    Pair with given sum optimized implementationation

--- src/main/java/com/practice/problem/solving/array/PairWithGivenSum.java
+++ src/main/java/com/practice/problem/solving/array/PairWithGivenSum.java
@@ -1,5 +1,6 @@
 package com.practice.problem.solving.array;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,9 +19,34 @@ public class PairWithGivenSum {
         return false;
     }
 
+    boolean findSumOfTwoOptimal(int[] A, int val) {
+        Arrays.sort(A);
+
+        int right = A.length - 1;
+        int left = 0;
+        int[] output = {};
+        while(left < right){
+            int sum = A[left] + A[right];
+
+            if(sum == val){
+                output = new int[]{left + 1, right + 1};
+                return true;
+            }
+
+            if(sum < val){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         PairWithGivenSum pairWithGivenSum = new PairWithGivenSum();
         int[] input = {2, 1, 8, 4, 7, 3};
         System.out.println(pairWithGivenSum.findSumOfTwo(input,11));
+        System.out.println(pairWithGivenSum.findSumOfTwoOptimal(input,11));
     }
 }
