9f063e36c1ed3b660f372219020e1038856a1c3d
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-03-25 16:44:25 +0530

    Two Pointers: Two Sum BST

--- src/main/java/com/practice/problem/solving/twopointers/TwoSumBST.java
+++ src/main/java/com/practice/problem/solving/twopointers/TwoSumBST.java
@@ -1,6 +1,8 @@
 package com.practice.problem.solving.twopointers;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class TwoSumBST {
 
@@ -39,5 +41,34 @@ public class TwoSumBST {
 
         set.add(root.val);
         return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
+
+    public boolean findTargetWithTwoPointers(TreeNode root, int k) {
+        List<Integer> inorderList = new ArrayList<>();
+        inorderTraversal(root, inorderList);
+        int left = 0, right = inorderList.size() - 1;
+
+        while (left < right){
+            int sum = inorderList.get(left) + inorderList.get(right);
+            if(sum == k){
+                return true;
+            } else if(sum < k){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> inorderList) {
+        if (root == null){
+            return;
+        }
+
+        inorderTraversal(root.left, inorderList);
+        inorderList.add(root.val);
+        inorderTraversal(root.right, inorderList);
     }
 }
