7e629a679afff4e61610a8d004ff52658a44579e
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-03-29 21:31:27 +0530

    Two Pointers: Sqaures of Sorted Array

---
+++ src/main/java/com/practice/problem/solving/twopointers/SquaresOfSortedArray.java
@@ -0,0 +1,24 @@
+package com.practice.problem.solving.twopointers;
+
+public class SquaresOfSortedArray {
+
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        int[] result = new int[nums.length];
+        int index = nums.length - 1;
+        while (left <= right){
+            if(Math.abs(nums[left]) >= Math.abs(nums[right])){
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else{
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+            index--;
+        }
+
+        return result;
+    }
+}
