ad012b91f071c8850b3b7fe6becad480d59fe6ff
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-04-11 16:57:51 +0530

    Fast And Slow Pointers: Detect Cycle

---
+++ src/main/java/com/practice/problem/solving/fastandslowpointers/DetectCycle.java
@@ -0,0 +1,34 @@
+package com.practice.problem.solving.fastandslowpointers;
+
+public class DetectCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
