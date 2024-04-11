2fc42f4442ca5c61cedacfb0183f6519b161abd1
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-04-11 17:08:52 +0530

    Two Pointer Approach: Find k closest Elements

---
+++ src/main/java/com/practice/problem/solving/twopointers/FindKClosestElements.java
@@ -0,0 +1,27 @@
+package com.practice.problem.solving.twopointers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (right - left >= k) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+}
