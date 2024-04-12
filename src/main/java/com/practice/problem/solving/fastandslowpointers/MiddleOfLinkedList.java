7be078dea868960c6ac5880bb07a56647a384b81
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-04-12 12:08:47 +0530

    Slow And Fast Pointers: Middle Of Linked List

---
+++ src/main/java/com/practice/problem/solving/fastandslowpointers/MiddleOfLinkedList.java
@@ -0,0 +1,30 @@
+package com.practice.problem.solving.fastandslowpointers;
+
+public class MiddleOfLinkedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
