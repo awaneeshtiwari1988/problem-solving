11fad4282cb56548c05238e1cd102299df183b6f
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2024-04-14 12:19:17 +0530

    Slow And Fast Pointer Approach: Duplicate Number

---
+++ src/main/java/com/practice/problem/solving/fastandslowpointers/DuplicateNumber.java
@@ -0,0 +1,27 @@
+package com.practice.problem.solving.fastandslowpointers;
+
+public class DuplicateNumber {
+
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+        slow = nums[0];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return fast;
+    }
+}
