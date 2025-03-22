86338c785905916c3eb32a6f6af592be15d75248
awaneeshtiwari1988 <awaneeshtiwari1988@gmail.com>
2025-03-22 14:12:30 +0530

    Sliding Window Max

--- src/main/java/com/practice/problem/solving/twopointers/SlidingWindowMaximum.java
+++ src/main/java/com/practice/problem/solving/twopointers/SlidingWindowMaximum.java
@@ -33,33 +33,6 @@ public class SlidingWindowMaximum {
         return output;
     }
 
-    public int[] maxSlidingWindowHeap(int[] nums, int k) {
-        if(nums == null || nums.length == 0){
-            return new int[0];
-        }
-
-        int[] output = new int[nums.length - k + 1];
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-
-        // Initial Window
-        for (int i = 0; i < k; i++) {
-            heap.offer(new int[]{nums[i],i});
-        }
-        output[0] = heap.peek()[0];
-        // Perform sliding window operation
-        for (int i = k; i < nums.length; i++) {
-            heap.offer(new int[]{nums[i],i});
-
-            while(heap.peek()[1] <= i - k){
-                heap.poll();
-            }
-
-            output[i-k+1] = heap.peek()[0];
-        }
-
-        return output;
-    }
-
     public static void main(String[] args) {
         int[] nums = {1,3,-1,-3,5,3,6,7};
         SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
